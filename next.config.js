fc6b5b261216d1625133d6f183b9142f209795b9
david <david.vangompel@gmail.com>
2024-06-26 08:41:34 +0200

    fix: force default basePath to /next

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const { i18n } = require("./next-i18next.config");
 const getPathsFromFolderStructure = require("./src/getPathsFromFolderStructure");
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "/next";
+const basePath = "/next";
 
 const paths = getPathsFromFolderStructure("./src/app");
 
