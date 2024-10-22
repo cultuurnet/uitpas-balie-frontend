7cea43318780a9099867059468f18d076fc55be3
Dmitriy Vostrikov <dmitriy.vostrikov@zinderlabs.com>
2024-10-22 11:34:02 +0200

    feat: add NavBarIcon, NavBarItemContainer, and NavBarTypography components
    refs: #UPS-4979

---
+++ src/mobile/layouts/navbar/components/NavBarIcon.tsx
@@ -0,0 +1,18 @@
+import { Box, SvgIconTypeMap, SxProps, useTheme } from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+
+type NavBarIconProps = {
+  icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+  sx?: SxProps;
+};
+
+export const NavBarIcon = ({ icon, sx }: NavBarIconProps) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      component={icon}
+      sx={{ fontSize: 24, color: theme.palette.neutral[0], ...sx }}
+    />
+  );
+};
