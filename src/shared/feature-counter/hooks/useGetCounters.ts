e6557d350ab4bbaf3ccf5d6b08bc560ea4a02997
david <david.vangompel@gmail.com>
2025-01-29 09:58:10 +0100

    feat: sort counters alphabetically + shared logic between mobile & web
    Refs #UPS-4869

---
+++ src/shared/feature-counter/hooks/useGetCounters.ts
@@ -0,0 +1,32 @@
+import { useGetPermissions } from "@/shared/lib/dataAccess";
+import { Counter } from "@/shared/feature-counter/context/CounterContext";
+
+export const useGetCounters = (lastCounterUsed: Counter, searchString = "") => {
+  const { data: allData, isSuccess, isLoading } = useGetPermissions();
+
+  const dataWithoutLastCounter =
+    allData?.data?.filter(
+      (permission) => permission.organizer.id !== lastCounterUsed?.id
+    ) ?? [];
+
+  const filteredData = searchString
+    ? dataWithoutLastCounter?.filter((organizer) =>
+        organizer.organizer.name
+          ?.toLowerCase()
+          .includes(searchString.toLowerCase())
+      )
+    : dataWithoutLastCounter;
+
+  const sortedData = filteredData
+    ? filteredData.toSorted((a, b) =>
+        a.organizer.name!.localeCompare(b.organizer.name!)
+      )
+    : [];
+
+  return {
+    allData,
+    data: sortedData,
+    isLoading,
+    isSuccess,
+  };
+};
