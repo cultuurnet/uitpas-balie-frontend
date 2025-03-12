e3d395aa21128d91fef8fb408aa344aded934b48
khauman <kevin.hauman@zinderlabs.com>
2025-03-12 15:07:33 +0100

    feat(wip): init pwa

--- next.config.js
+++ next.config.js
@@ -37,6 +37,27 @@ const nextConfig = {
     },
   },
   transpilePackages: ["@tanstack/query-core", "@tanstack/react-query"],
+  async headers(){
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/javascript; charset=utf-8',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self'",
+          },
+        ],
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
