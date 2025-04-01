0259d3173ae5c1ed10af21245bfab49d97630c7d
david <david.vangompel@gmail.com>
2025-04-01 15:17:58 +0200

    chore: upgrade next to 15.2.4

--- src/app/MobileProviders.tsx
+++ src/app/MobileProviders.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { PropsWithChildren, useEffect, useState } from "react";
+import { PropsWithChildren } from "react";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { AuthProvider } from "@/shared/lib/auth";
