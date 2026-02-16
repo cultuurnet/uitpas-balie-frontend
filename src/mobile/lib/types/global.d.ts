624f7d5ac67710d6f0e2ca4ecf82bf8875b732df
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2026-02-16 12:01:40 +0100

    Fix types check

--- src/mobile/lib/types/global.d.ts
+++ src/mobile/lib/types/global.d.ts
@@ -1,3 +1,13 @@
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.svg?url' {
+  const content: string;
+  export default content;
+}
+
 declare global {
   interface MediaTrackCapabilities {
     torch?: boolean;
