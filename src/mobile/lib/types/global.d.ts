45689895e1e8731252ae414c50f6e4865299d887
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2026-02-16 14:09:45 +0100

    Remove from global.d.ts

--- src/mobile/lib/types/global.d.ts
+++ src/mobile/lib/types/global.d.ts
@@ -1,13 +1,3 @@
-declare module '*.svg' {
-  const content: any;
-  export default content;
-}
-
-declare module '*.svg?url' {
-  const content: string;
-  export default content;
-}
-
 declare global {
   interface MediaTrackCapabilities {
     torch?: boolean;
