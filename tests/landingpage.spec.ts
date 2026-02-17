b9035a551ebc932868f60d1fdd6fed98fbb1c83c
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2026-02-17 15:44:05 +0100

    UPS-5872 - Add test assertions

--- tests/landingpage.spec.ts
+++ tests/landingpage.spec.ts
@@ -1,5 +1,52 @@
 import { test, expect } from '@playwright/test';
 
-test('Go to landingpage', async ({ page }) => {
-  await page.screenshot({ path: 'landing.png' });
+test('Go to landingpage', async ({ page, baseURL }) => {
+  await page.goto(`${baseURL}/app`);
+
+  await page.waitForLoadState('networkidle');
+  await page.waitForLoadState('domcontentloaded');
+
+  await expect(
+    page.getByRole('heading', { name: 'selecteer je balie' })
+  ).toBeVisible();
+
+  await expect(page.getByRole('button', { name: 'publiq vzw' })).toBeVisible();
+
+  // Select counter
+  await page.getByRole('button', { name: 'muntpunt' }).click();
+
+  // Counter detail
+  await expect(
+    page.getByRole('heading', { name: 'kies een activiteit' })
+  ).toBeVisible();
+
+  await page.getByRole('button', { name: 'kom naar muntpunt' }).click();
+
+  // Event detail
+  await expect(
+    page.getByRole('heading', { name: 'gekozen activiteit' })
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole('heading', { name: 'kom naar muntpunt' })
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole('button', { name: 'barcode scannen' })
+  ).toBeVisible();
+
+  // TODO make the pass number configurable
+  await page.getByPlaceholder('Kaartnummer of RRN').fill('55100100130');
+
+  await page.getByRole('button', { name: 'bevestig' }).click();
+
+  await expect(
+    page.getByRole('heading', { name: 'Jean-Marie Hoffelinck' })
+  ).toBeVisible();
+
+  await expect(page.getByText('punt gespaard')).toBeVisible();
+
+  await expect(
+    page.getByRole('button', { name: 'Volgende uitpas scannen' })
+  ).toBeVisible();
 });
