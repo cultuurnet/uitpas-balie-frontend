26a43c62ee199ccdee36a59c7f887d271d54f97a
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2026-02-17 13:46:21 +0100

    UPS-5872 - Setup minimal landing page test

--- tests/landingpage.spec.ts
+++ tests/landingpage.spec.ts
@@ -1,16 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('Go to landingpage', async ({ page, baseURL }) => {
-  if (!baseURL) {
-    return;
-  }
-
-  await page.goto(`${baseURL}/login?redirectTo=/`);
-
-  await page.waitForLoadState('networkidle');
-  await page.waitForLoadState('domcontentloaded');
-
-  await expect(
-    page.getByRole('heading', { name: 'mobiele balie' })
-  ).toBeVisible();
+test('Go to landingpage', async ({ page }) => {
+  await page.screenshot({ path: 'landing.png' });
 });
