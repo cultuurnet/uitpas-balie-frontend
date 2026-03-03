a4ce57b7262ee10bef341c3907e84ee4597bd4a4
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2026-03-03 14:34:46 +0100

    Use new initFetch instead of initAxios

--- src/shared/feature-config/context/ConfigProvider.tsx
+++ src/shared/feature-config/context/ConfigProvider.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren, useEffect, useState } from 'react';
 import { ConfigContext } from './ConfigContext';
 import { PublicRuntimeConfig } from '../types';
 import { getConfig } from '../getConfig';
-import { initAxios } from '@/shared/lib/dataAccess/initAxios';
+import { initFetch } from '@/shared/lib/dataAccess/initFetch';
 import { initUrlUtils } from '@/shared/lib/utils';
 
 type ConfigProviderProps = {};
@@ -19,7 +19,7 @@ export function ConfigProvider({
   useEffect(() => {
     getConfig().then((config) => {
       setConfig(config);
-      initAxios(config);
+      initFetch(config);
       initUrlUtils(config);
     });
   }, []);
