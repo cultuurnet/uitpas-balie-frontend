dae3e21376ae2f13b14ba1c506dd36ee53510c1b
Sander Vleugels <sandervleugels@gmail.com>
2026-03-04 09:40:36 +0100

    UPS-5867: add base shadcn components for input group and spinner

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -10,5 +10,16 @@ export {
   CardContent,
 } from './Card';
 export type { CardProps } from './Card';
+export {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupText,
+  InputGroupInput,
+  InputGroupTextarea,
+} from './InputGroup';
+export type { InputGroupProps } from './InputGroup';
 export { Link } from './Link';
 export type { LinkProps } from './Link';
+export { Spinner } from './Spinner';
+export type { SpinnerProps } from './Spinner';
