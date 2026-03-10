e55c37a6e3baa6f5423eccfda0f77617c31c1b8c
Sander Vleugels <sandervleugels@gmail.com>
2026-03-10 16:02:48 +0100

    UPS-5867: change playwright reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,22 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI
+    ? [
+        ['list'], // Shows each test as it runs
+        ['github'], // GitHub Actions annotations
+        ['html'], // Generates HTML report
+        [
+          'json',
+          {
+            outputFile: 'playwright-report/test-results.json',
+          },
+        ],
+      ]
+    : [
+        ['list'], // Shows each test as it runs
+        ['html'], // Generates HTML report
+      ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
