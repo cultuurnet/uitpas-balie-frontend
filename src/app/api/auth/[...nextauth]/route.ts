715547039dee6a5207629b2789ee3eb081429f24
Bram Pauwelyn <brampauwelyn@users.noreply.github.com>
2026-03-16 15:13:29 +0100

    UPS-5864 - Add refreshToken to jwt callback

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -17,9 +17,10 @@ const handler = NextAuth({
   ],
   callbacks: {
     async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.expiresAt = account.expires_at;
       }
       return token;
     },
