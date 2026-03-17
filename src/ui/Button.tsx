869258a13b722d83edd9ed59a417a47354560d06
Sander Vleugels <sandervleugels@gmail.com>
2026-03-17 13:02:02 +0100

    UPS-5868: set cursor as pointer more globally

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,12 +1,8 @@
 import type { ComponentProps } from 'react';
 
 import { Button as ShadcnButton, buttonVariants } from '@/ui/shadcn/button';
-import { cn } from '@/utils/shadcn';
-
 export type ButtonProps = ComponentProps<typeof ShadcnButton>;
 
-export const Button = ({ className, ...props }: ButtonProps) => (
-  <ShadcnButton className={cn('cursor-pointer', className)} {...props} />
-);
+export const Button = (props: ButtonProps) => <ShadcnButton {...props} />;
 
 export { buttonVariants };
